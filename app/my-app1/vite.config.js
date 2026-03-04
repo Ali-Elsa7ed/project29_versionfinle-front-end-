a080bef4cef6fce02c5e1c35e51da8ba5353ad07
Admin <admin@example.com>
2026-03-04 22:08:39 +0200

    Fix: Update vite base path to match GitHub Pages URL

--- app/my-app1/vite.config.js
+++ app/my-app1/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/project29_version0/",
+  base: "/project29_versionfinle-front-end-/",
   plugins: [react(), tailwindcss()],
 })
